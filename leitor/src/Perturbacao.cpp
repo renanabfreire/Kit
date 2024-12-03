1316970c78edaec88f50f5d0fb72fe68615e01d6
Renan Abreu B. Freire <renan.freire@academico.ufpb.br>
2024-12-03 16:39:54 -0300

    Pertubacao Correcao

--- leitor/src/Perturbacao.cpp
+++ leitor/src/Perturbacao.cpp
@@ -10,16 +10,13 @@ Solution Perturbacao(Solution s, Data &d)
 
     while (true)
     {
-        if(sindice == indice)
-            sindice = 1 + rand()%(s.sequence.size()-stam-1);
-        else if (sindice < indice && (sindice+stam) > indice)
-            sindice = 1 + rand()%(s.sequence.size()-stam-1);
-        else if (sindice < (indice+tam) && (sindice+stam) >= (indice + tam))
-            sindice = 1 + rand()%(s.sequence.size()-stam-1);
-        else if (sindice > indice && (sindice+stam) <= (indice+tam))
-            sindice = 1 + rand()%(s.sequence.size()-stam-1);
-        else if(sindice < indice && (sindice+stam) >= (indice+tam))
-            sindice = 1 + rand()%(s.sequence.size()-stam-1);
+        if(sindice < indice)
+        {
+            std::swap(indice, sindice);
+            std::swap(tam, stam);
+        }
+        else if(sindice == indice || (indice + tam) > sindice)
+            sindice = 1 + rand()%int((s.sequence.size()-stam-2));
         else
             break;
     }
@@ -39,47 +36,24 @@ Solution Perturbacao(Solution s, Data &d)
     else if (next_segundo == primeiro)
         delta =  - d.getDistance(prev_segundo, segundo) + d.getDistance(prev_segundo, primeiro) - d.getDistance(end_segundo, primeiro) + d.getDistance(end_primeiro, segundo) - d.getDistance(end_primeiro, next_primeiro) + d.getDistance(end_segundo, next_primeiro);
     
-    //cout << prev_primeiro << " - " << segundo << " - " << end_segundo << " - " << next_primeiro << " - "<< prev_segundo << " - " << primeiro << " - " << end_primeiro << " - " << next_segundo << endl;
-    
     std::vector<int> inv(tam);
-    //cout << tam << endl;
     std::vector<int> sinv(stam);
-    //cout << stam << endl;
-   
     for(int i=0; i<tam; i++)
     {
         inv[i] = s.sequence[indice];
         s.sequence.erase(s.sequence.begin()+indice);
     }
-    if(indice < sindice)
+    for(int i=0; i<stam; i++)
     {
-        for(int i=0; i<stam; i++)
-        {
-            sinv[i] = s.sequence[sindice-tam];
-            s.sequence.erase(s.sequence.begin()+sindice-tam);
-        }
-        for(int i=0; i<tam; i++)
-            s.sequence.insert(s.sequence.begin()+sindice-tam+i, inv[i]);
-        for(int i=0; i<stam; i++)
-            s.sequence.insert(s.sequence.begin()+indice+i, sinv[i]);
+        sinv[i] = s.sequence[sindice-tam];
+        s.sequence.erase(s.sequence.begin()+sindice-tam);
     }
-    else
-    {
-        for(int i=0; i<stam; i++)
-        {
-            sinv[i] = s.sequence[sindice];
-            s.sequence.erase(s.sequence.begin()+sindice);
-        }
-        for(int i=0; i<stam; i++)
-            s.sequence.insert(s.sequence.begin()+indice-stam+i, sinv[i]);
-        for(int i=0; i<tam; i++)
-            s.sequence.insert(s.sequence.begin()+sindice+i, inv[i]);
-    }
-
+    for(int i=0; i<tam; i++)
+        s.sequence.insert(s.sequence.begin()+sindice-tam+i, inv[i]);
+    for(int i=0; i<stam; i++)
+        s.sequence.insert(s.sequence.begin()+indice+i, sinv[i]);
 
     s.value += delta;
-
-    //cout << delta << endl;
 
     return s;
 }
