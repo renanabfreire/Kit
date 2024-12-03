6ac8d0be77f7929ce33b6a28a2691a5222c6b97f
Renan Abreu B. Freire <renan.freire@academico.ufpb.br>
2024-12-03 13:41:32 -0300

    BuscaLocal comentarios

--- leitor/src/BuscaLocal.cpp
+++ leitor/src/BuscaLocal.cpp
@@ -15,29 +15,30 @@ void BuscaLocal(Solution &s, Data &d)
         switch(NL[n])
         {
         case 1:
-            improved = bestImprovementSwap(s, d);
+            improved = bestImprovementSwap(s, d); // Swap
             break;
         case 2:
-            improved = bestImprovement2Opt(s, d);
+            improved = bestImprovement2Opt(s, d); // 2-opt
             break;
         case 3:
-            improved = bestImprovementOrOpt(s, d, 1);
+            improved = bestImprovementOrOpt(s, d, 1); // Reinsertion
             break;
         case 4:
-            improved = bestImprovementOrOpt(s, d, 2);
+            improved = bestImprovementOrOpt(s, d, 2); // Or-opt-2
             break;
         case 5:
-            improved = bestImprovementOrOpt(s, d, 3);
+            improved = bestImprovementOrOpt(s, d, 3); // Or-opt-3
             break;
         }
 
         if(improved)
-            NL = {1, 2, 3, 4, 5};
+            NL = {1, 2, 3, 4, 5}; //repetindo processo em caso de melhora
         else
-            NL.erase(NL.begin() + n);
+            NL.erase(NL.begin() + n); // eliminando operação que não obteve melhora
     }
 }
 
+// Swap
 bool bestImprovementSwap(Solution &s, Data &d)
 {
     double bestDelta=0;
@@ -45,22 +46,23 @@ bool bestImprovementSwap(Solution &s, Data &d)
     
     for(int i=1; i<s.sequence.size() - 2; i++)
     {
-        int vi = s.sequence[i];
+        int vi = s.sequence[i]; // Percorrendo primeiro nó para troca
         int vi_next = s.sequence[i+1];
         int vi_prev = s.sequence[i-1];
         
         for(int j=i+1; j<s.sequence.size() - 1; j++)
         {
-            int vj = s.sequence[j];
+            int vj = s.sequence[j]; // Segundo nó para troca
             int vj_next = s.sequence[j+1];
             int vj_prev = s.sequence[j-1];
+            // Calculando variação de custo
             double delta = -d.getDistance(vi_prev, vi) - d.getDistance(vi, vi_next)+d.getDistance(vi_prev, vj) + d.getDistance(vj, vi_next)-d.getDistance(vj_prev, vj) - d.getDistance(vj, vj_next)+d.getDistance(vj_prev, vi) + d.getDistance(vi, vj_next);
 
+            // Para o caso dos vertices pertencerem a uma mesma aresta, a aresta é eliminada 2 vezes, mas deve ser considerada
             if(vi_next == vj)
-            {
                 delta += 2 * d.getDistance(vi, vj);
-            }
 
+            // Atualizando melhores valores
             if(delta < bestDelta)
             {
                 bestDelta = delta;
@@ -70,6 +72,7 @@ bool bestImprovementSwap(Solution &s, Data &d)
         }
     }
 
+    // Atualizando a solução para o melhor caso
     if(bestDelta < 0)
     {
         std::swap(s.sequence[best_i], s.sequence[best_j]);
@@ -79,6 +82,7 @@ bool bestImprovementSwap(Solution &s, Data &d)
     return false;
 }
 
+// 2-opt
 bool bestImprovement2Opt(Solution &s, Data &d)
 {
     double bestDelta = 0;
@@ -86,15 +90,17 @@ bool bestImprovement2Opt(Solution &s, Data &d)
 
     for(int a=1; a<s.sequence.size() - 2; a++)
     {
-        int va = s.sequence[a];
+        int va = s.sequence[a]; // Percorrendo arestas
         int va_next = s.sequence[a+1];
         
         for(int b=a+2; b<s.sequence.size() -2; b++)
         {
-            int vb = s.sequence[b];
+            int vb = s.sequence[b]; // Percorrendo a outra aresta que será retirada
             int vb_next = s.sequence[b+1];
+            // Calculando variação do custo
             double delta = -d.getDistance(va, va_next) - d.getDistance(vb, vb_next) + d.getDistance(va, vb) + d.getDistance(va_next, vb_next);
             
+            // Atualizando melhores valores
             if(delta < bestDelta)
             {
                 bestDelta = delta;
@@ -104,10 +110,12 @@ bool bestImprovement2Opt(Solution &s, Data &d)
         }
     }
 
+    // Atualizando a solução
     if(bestDelta < 0)
     {
         int l=0;
     
+        // invertendo trecho entre as arestas retiradas
         for(int i=best_a+1; i<=float((best_a+best_b)/2); i++)
         {
             std::swap(s.sequence[i], s.sequence[best_b-l]);
@@ -121,6 +129,7 @@ bool bestImprovement2Opt(Solution &s, Data &d)
     return false;
 }
 
+// Or-opt e Reinsertion
 bool bestImprovementOrOpt(Solution &s, Data &d, int numeroDeNos)
 {
     double bestDelta = 0;
@@ -128,17 +137,18 @@ bool bestImprovementOrOpt(Solution &s, Data &d, int numeroDeNos)
 
     for(int a=1; a<s.sequence.size() - 2 - numeroDeNos; a++)
     {
-        int va1 = s.sequence[a];
+        int va1 = s.sequence[a]; // verificando arestas removidas, isolando o bloco de até 3 nós
         int va1_next = s.sequence[a+1];
         int va2 = s.sequence[a+numeroDeNos];
         int va2_next = s.sequence[a+1+numeroDeNos];
         
         for(int b=a+2+numeroDeNos; b<s.sequence.size() -1; b++)
         {
-            int vb = s.sequence[b];
+            int vb = s.sequence[b]; // verificando aresta que será aberta para reinserção do bloco isolado
             int vb_next = s.sequence[b+1];
             double delta = - d.getDistance(va1, va1_next) - d.getDistance(va2, va2_next) + d.getDistance(va1, va2_next) - d.getDistance(vb, vb_next) + d.getDistance(vb, va2) + d.getDistance(va1_next, vb_next);
 
+            // Atualizando melhores valores
             if(delta < bestDelta)
             {
                 bestDelta = delta;
@@ -162,13 +172,13 @@ bool bestImprovementOrOpt(Solution &s, Data &d, int numeroDeNos)
         }*/
     }
 
-    //cout << bestDelta << endl;
-
+    // Atualizando solução
     if(bestDelta < 0)
     {
         std::vector<int> inversao(numeroDeNos);
         int l=0;
 
+        // Isolando bloco
         for(int i=best_a+1; i<=best_a+numeroDeNos; i++)
         {
             inversao[l] = s.sequence[best_a+1];
@@ -176,6 +186,7 @@ bool bestImprovementOrOpt(Solution &s, Data &d, int numeroDeNos)
             l++;
         }
 
+        // Reinserindo o bloco de maneira invertida no lugar da aresta aberta
         l=numeroDeNos-1;
         for(int i=best_b; i<best_b+numeroDeNos; i++)
         {
--- leitor/src/Ils.cpp
+++ leitor/src/Ils.cpp
@@ -6,30 +6,27 @@ Solution ILS(int maxIter, int maxIterIls, Data &data)
     bestOfAll.value = INFINITY;
     for(int i=0; i<maxIter; i++)
     {
-        Solution s = Construcao(data);
-        //cout << "constuido" << s.value << endl;
+        Solution s = Construcao(data); // Gerando palpites
         Solution best = s;
 
         int iterIls = 0;
 
         while (iterIls <= maxIterIls)
         {
-            BuscaLocal(s, data);
+            BuscaLocal(s, data); // Melhorando solução
+            // Atualizando melhores valores
             if(s.value < best.value)
             {
                 best = s;
                 iterIls = 0;
-                //cout << "Busca local: " << s.value << endl;
             }
-            s = Perturbacao(best, data);
-            //cout << "Perturbacao: " << s.value << endl;
+            s = Perturbacao(best, data); // Copia da melhor versão levemente modificada
             iterIls++;
         }
-        //cout << best.value << endl;
+        // atualizando a melhor solução
         if(best.value < bestOfAll.value)
             bestOfAll = best;
 
-        //cout << bestOfAll.value << endl;
     }
 
     return bestOfAll;
