51ac20c3a22df0ea6a0f392b9133b6a7214ae003
renanabfreire <renanabfreire@gmail.com>
2025-02-25 22:45:09 -0300

    Branch-and-Bound

---
+++ Branch-and-Bound/leitor/obj/main.d
@@ -0,0 +1,2 @@
+obj/main.o: src/main.cpp src/BB.h src/data.h src/node.h src/Solution.h \
+ src/hungarian.h
---
+++ Branch-and-Bound/leitor/obj/solve_node.d
@@ -0,0 +1,2 @@
+obj/solve_node.o: src/solve_node.cpp src/BB.h src/data.h src/node.h \
+ src/Solution.h src/hungarian.h
